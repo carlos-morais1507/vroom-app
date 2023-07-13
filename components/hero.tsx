e482fefc54b98a9d9418cc556b71f767ed2e1a4d
c.morais <c.morais1507@gmail.com>
2023-07-12 21:34:02 -0300

    :sparkles: feat: Finish Hero Section

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,12 +1,18 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Hero = () => {
   return (
-    <div>
+    <div className='h-[435px] w-full flex items-center justify-between'>
       <div>
-        <h2>Quer comprar o seu carro com segurança?</h2>
-        <h1 className='text-primary text-xl font-bold'>Vê na Vroom!</h1>
-      </div>
+        <div className='prose prose-h1:text-primary'>
+          <h2>Quer comprar o seu carro com segurança?</h2>
+          <h1 className='-my-3'>Vê na Vroom!</h1>
+          <h4>Nós temos a solução pro seu problema!</h4>
+        </div>
+        <Link href="#" className='btn btn-primary mt-5'>Carros à venda</Link>
+        </div>
+          <img src="/hero.png" alt="Hero" className='object-contain w-[60%]'/>
     </div>
   )
 }
