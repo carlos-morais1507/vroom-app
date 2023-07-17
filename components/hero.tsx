440866e4ad59dd20964f15037417bf70154b6376
c.morais <c.morais1507@gmail.com>
2023-07-17 19:27:43 -0300

    :iphone: feat: Responsive Design Fixes

--- components/hero.tsx
+++ components/hero.tsx
@@ -3,16 +3,16 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='h-[435px] w-full flex items-center justify-between'>
+    <div className='h-[435px] w-full flex sm:flex-row flex-col items-center justify-between'>
       <div>
-        <div className='prose prose-h1:text-primary'>
-          <h2>Quer comprar o seu carro com segurança?</h2>
-          <h1 className='-my-3'>Vê na Vroom!</h1>
-          <h4>Nós temos a solução pro seu problema!</h4>
+        <div>
+          <h2 className='sm:text-lg font-semibold'>Quer comprar o seu carro com segurança?</h2>
+          <h1 className='text-primary font-bold sm:text-5xl text-3xl sm:mt-0 mt-3'>Vê na Vroom!</h1>
+          <h4 className='sm:text-base text-sm'>Nós temos a solução pro seu problema!</h4>
         </div>
-        <Link href="#" className='btn btn-primary mt-5'>Carros à venda</Link>
+        <Link href="#" className='btn btn-primary mt-5 tracking-wider'>Carros à venda</Link>
         </div>
-          <img src="/hero.png" alt="Hero" className='object-contain w-[60%]'/>
+          <img src="/hero.png" alt="Hero" className='object-contain sm:w-[60%]'/>
     </div>
   )
 }
