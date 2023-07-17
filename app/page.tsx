c05f1ca9b00e5ad4cd0ffb70dcf548b813a02311
c.morais <c.morais1507@gmail.com>
2023-07-17 20:31:41 -0300

    :sparkles: feat: Add Card Group Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import { Hero } from '@/components'
+import { CardGroup, Hero } from '@/components'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between sm:p-24 p-12">
+    <main className="flex min-h-screen flex-col items-center justify-between sm:p-24 p-12 overflow-x-hidden">
       <Hero />
+      <CardGroup />
     </main>
   )
 }
