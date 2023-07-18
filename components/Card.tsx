fb900239106c21390d73c1bda9144e561f56f2b9
c.morais <c.morais1507@gmail.com>
2023-07-18 13:04:21 -0300

    :lipstick: Minor Changes

--- components/Card.tsx
+++ components/Card.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const Card = () => {
   return (
     <div className='card shadow-xl w-64'>
-      <figure><Image src="https://www.honda.com.br/automoveis/sites/hab/files/2023-01/004_Design.jpg" width={250} height={250} alt='Produto'/></figure>
+      <figure><Image src="https://www.honda.com.br/automoveis/sites/hab/files/2023-01/004_Design.jpg" width={280} height={280} alt='Produto'/></figure>
       <div className='card-body'>
         <h2 className='card-title'>Honda Civic 2022</h2>
         <h3>Rio de Janeiro - RJ</h3>
