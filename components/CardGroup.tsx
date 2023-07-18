6ea9aa99b750db9d2f20125cd35b535d7c277b88
c.morais <c.morais1507@gmail.com>
2023-07-18 10:29:05 -0300

    :iphone: feat: Responsive Design Fixes

--- components/CardGroup.tsx
+++ components/CardGroup.tsx
@@ -5,7 +5,7 @@ const CardGroup = () => {
   return (
     <div className='max-w-full'>
       <h1 className='font-bold text-2xl'>Mais Vistos</h1>
-      <div className='grid grid-flow-col sm:auto-cols-[27%] auto-cols-[88%] gap-3 overflow-x-auto overscroll-contain scroll py-10 horizontal scroll-p-3 -mt-5'>
+      <div className='grid grid-flow-col auto-cols-auto gap-3 overflow-x-auto overscroll-contain scroll py-10 horizontal scroll-p-3 -mt-5'>
         <Card />
         <Card />
         <Card />
@@ -15,7 +15,7 @@ const CardGroup = () => {
         <Card />
       </div>
       <h1 className='font-bold text-2xl mt-7'>Novidades</h1>
-      <div className='grid grid-flow-col sm:auto-cols-[27%] auto-cols-[88%] gap-3 overflow-x-auto overscroll-contain scroll py-10 horizontal scroll-p-3 -mt-5'>
+      <div className='grid grid-flow-col auto-cols-auto gap-3 overflow-x-auto overscroll-contain scroll py-10 horizontal scroll-p-3 -mt-5'>
         <Card />
         <Card />
         <Card />
