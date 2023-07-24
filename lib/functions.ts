e56d8695498b30c3e8d01503f996cb398b7a7efc
c.morais <c.morais1507@gmail.com>
2023-07-24 20:11:19 -0300

    :sparkles: feat: New OAuth Signin

---
+++ lib/functions.ts
@@ -0,0 +1,8 @@
+import { auth } from '@/lib/firebase'
+import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
+
+export async function signInWithGoogle() {
+  const provider = new GoogleAuthProvider();
+  const user = await signInWithPopup(auth, provider);
+  console.log(user)
+}
